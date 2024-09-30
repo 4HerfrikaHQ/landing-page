55893323c1d6715f676c625424562d5c1c4df4ee
elcharitas <jonathanirhodia@gmail.com>
2024-09-30 09:40:01 +0100

    chore: setup layout and styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,40 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+                primary: {
+                    100: "var(--primary-100)",
+                    200: "var(--primary-200)",
+                    300: "var(--primary-300)",
+                    400: "var(--primary-400)",
+                    500: "var(--primary-500)",
+                },
+                secondary: {
+                    100: "var(--secondary-100)",
+                    200: "var(--secondary-200)",
+                    300: "var(--secondary-300)",
+                    400: "var(--secondary-400)",
+                    500: "var(--secondary-500)",
+                },
+                neutral: {
+                    100: "var(--neutral-100)",
+                    200: "var(--neutral-200)",
+                    300: "var(--neutral-300)",
+                    400: "var(--neutral-400)",
+                    500: "var(--neutral-500)",
+                },
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
