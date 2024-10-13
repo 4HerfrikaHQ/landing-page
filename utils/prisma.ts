924a0e697bb80c5e9ad61b23fdff6dbe33015190
elcharitas <jonathanirhodia@gmail.com>
2024-10-13 23:29:45 +0100

    chore: add packages and fix prisma

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -1,8 +1,10 @@
 import { PrismaClient } from "@prisma/client";
+import { getRequestContext } from "@cloudflare/next-on-pages";
 import { PrismaD1 } from "@prisma/adapter-d1";
 
-export function prisma(env?: CloudflareEnv) {
-  if (env && env.ENV === "production") {
+export function prisma(): PrismaClient {
+  if (process.env.ENV === "production") {
+    const { env } = getRequestContext();
     const adapter = new PrismaD1(env.DB);
     return new PrismaClient({ adapter });
   }
