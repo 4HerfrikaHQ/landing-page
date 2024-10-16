04423176434fd28e81f800db764352824dffbfa5
elcharitas <jonathanirhodia@gmail.com>
2024-10-16 20:40:45 +0100

    chore: switch to openext

--- app/(admin)/api/admin/[[...all]]/route.ts
+++ app/(admin)/api/admin/[[...all]]/route.ts
@@ -3,13 +3,12 @@ import { options } from "@/app/(admin)/backoffice/options";
 import schema from "@/prisma/json-schema/json-schema.json";
 import { createHandler } from "@premieroctet/next-admin/appHandler";
 
-export const runtime = "edge";
-
 const { run } = createHandler({
   apiBasePath: "/api/admin",
   options,
   prisma,
   schema,
+  paramKey: "all",
 });
 
 export { run as DELETE, run as GET, run as POST };
