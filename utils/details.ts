ae1e4e915cc984ee8e28f2f78a0f3b0008ff215d
Fasina Ayomikun <101244317+Fasina-ayomikun@users.noreply.github.com>
2024-11-20 19:18:24 +0100

    feat: add careers corner page (#13)

---
+++ utils/details.ts
@@ -0,0 +1,57 @@
+export const counsellingMentors = [
+  {
+    name: "Odunola victor-ayegba",
+    position: "business consultant",
+    image: "/assets/careers/Odunola.png",
+  },
+  {
+    name: "Ruphina H.",
+    position: "product designer",
+    image: "/assets/careers/Ruphina.png",
+  },
+  {
+    name: "sosanya olumide",
+    position: "senior software engineer",
+    image: "/assets/careers/sosanya.png",
+  },
+  {
+    name: "sharon",
+    position: "Personal Branding Expert and LinkedIn Strategist",
+    image: "/assets/careers/sharon.png",
+  },
+  {
+    name: "Hassanat abubakar",
+    position: "Product manager",
+    image: "/assets/careers/Hassanat.png",
+  },
+  {
+    name: "Isaac Nwachukwu",
+    position: "visual designer",
+    image: "/assets/careers/Isaac.png",
+  },
+  {
+    name: "Jonathan Irhodia",
+    position: "Senior Software Engineer",
+    image: "/assets/careers/Jonathan.png",
+  },
+  {
+    name: "dolapo oshikoya",
+    position: "Product manager",
+    image: "/assets/careers/Dolapo.png",
+  },
+  {
+    name: "Tomide",
+    position: "Mobile engineer",
+    image: "/assets/careers/Tomide.png",
+  },
+  {
+    name: "Husseinah K. Abubakar",
+    position: "content writer",
+    image: "/assets/careers/Husseinah.png",
+  },
+  {
+    name: "Oolayinka",
+    position: "Devrel engineer",
+    image: "/assets/careers/Yinka.png",
+  },
+];
