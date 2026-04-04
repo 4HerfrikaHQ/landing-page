c96b12369caf0b54124507ac6c0ec3059470983e
Oluwanifemi Adeyemi <deyeminifemi@gmail.com>
2026-04-04 20:06:34 +0200

    Add mentor with availability type to db file

--- app/[locale]/(website)/careers-corner/_actions.ts
+++ app/[locale]/(website)/careers-corner/_actions.ts
@@ -1,12 +1,7 @@
 "use server";
 import { db } from "@/src/db";
 import { schema } from "@/src/db";
-import { DbAvailability, DbMentor } from "@/src/db/schema/tables";
 import { eq } from "drizzle-orm";
-
-export type MentorWithAvailability = DbMentor & {
-  availability: DbAvailability[]
-};
 
 export async function getMentors() {
 	return db.query.mentors.findMany({
