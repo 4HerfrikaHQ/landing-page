ef1625c1c402eb0b356db633fc464b21a4fd6796
Oluwanifemi Adeyemi <36075575+Oluwa-nifemi@users.noreply.github.com>
2026-07-20 13:27:57 +0100

    Link Join Us directly to application form

--- app/[locale]/(website)/navigation.ts
+++ app/[locale]/(website)/navigation.ts
@@ -9,9 +9,12 @@ export const NAV_LINKS = [
 	{ name: "Contact Us", href: "/contact-us" },
 ];
 
+export const JOIN_US_URL =
+	"https://docs.google.com/forms/d/e/1FAIpQLSdAc4tbTbcdh1CIjO0C8eoSL7bNWzBlT1aykd8Vcm67PDnprw/viewform";
+
 export const ACTION_BUTTONS = [
-	{ name: "Donate", href: "/donate", isPrimary: false },
-	{ name: "Join Us", href: "/join-us", isPrimary: true },
+	{ name: "Donate", href: "/donate", isPrimary: false, isExternal: false },
+	{ name: "Join Us", href: JOIN_US_URL, isPrimary: true, isExternal: true },
 ];
 
 export type Navlink = (typeof NAV_LINKS)[number] & {
